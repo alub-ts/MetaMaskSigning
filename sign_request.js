3802be24b8dfe987a46b4b4afafaf0c6f42a4bb6
alub-ts <56005618+alub-ts@users.noreply.github.com>
2019-10-02 19:25:37 -0400

    Update sign_request.js

--- sign_request.js
+++ sign_request.js
@@ -1,6 +1,5 @@
 const Web3 = require(web3);
 
-function signRequest() {
-    web3.personal.sign;
-    document.getElementById('requestSignature').style.display = 'block';
-}
+web3.personal.sign;
+document.getElementById('requestSignature').style.display = 'block';
+
